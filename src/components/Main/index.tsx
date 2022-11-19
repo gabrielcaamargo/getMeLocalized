85cf4ea778bf892c3235283962656b54bcb3395a
Gabriel <gcamargo.dev@gmail.com>
2022-11-19 17:13:26 -0300

    📦 chore: responsivity

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,8 @@
 import { MainContainer, SearchContainer, MapContainer, ResultsContainer, Results } from './styles';
 
+import map from '../../assets/mocks/map.svg';
+import divisor from '../../assets/icons/divisor.svg';
+
 export default function Main() {
   return (
     <MainContainer>
@@ -7,6 +10,24 @@ export default function Main() {
         <input type="text" placeholder='Search a place'/>
         <button type="button">Search</button>
       </SearchContainer>
+
+      <MapContainer>
+        <img src={map}/>
+      </MapContainer>
+
+      <ResultsContainer>
+        <Results>
+          <p><strong>Location: </strong> Porto Alegre</p>
+          <p><strong>Address: </strong>Not informed</p>
+          <p><strong>Country: </strong> Brazil 🇧🇷</p>
+        </Results>
+        <img src={divisor} alt="Divisor" />
+        <Results>
+          <p><strong>Places: </strong> Barranco, Parcão, Redenção...</p>
+          <p><strong>Postal Code: </strong>Not informed</p>
+          <p><strong>Region: </strong>South Brazil</p>
+        </Results>
+      </ResultsContainer>
     </MainContainer>
   );
 }
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -52,10 +52,68 @@ export const SearchContainer = styled.div`
   @media screen and (max-width: 640px) {
     grid-template-columns: 1fr;
   }
+
+  @media screen and (max-width: 400px) {
+    max-width: 90vw;
+    width: 100%;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
 `;
 
-export const MapContainer = styled.div``;
+export const MapContainer = styled.div`
+  margin-top: 44px;
+  height: 356px;
+  background-color: #E6E6E6;
+  img {
+    display: none;
+  }
+`;
 
-export const ResultsContainer = styled.div``;
+export const ResultsContainer = styled.div`
+  background-color: #E6E6E6;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1024px;
+  margin: 28px auto 0;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
 
-export const Results = styled.div``;
+  @media screen and (max-width: 1024px) {
+    padding: 20px;
+    max-width: 92vw;
+  }
+
+  @media screen and (max-width: 640px) {
+    flex-direction: column;
+    padding: 20px 12px;
+    align-items: start;
+    & > img {
+      display: none;
+    }
+  }
+`;
+
+export const Results = styled.div`
+  font-size: 18px;
+  width: 318px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  p {
+    height: 32px;
+  }
+
+  @media screen and (max-width: 640px) {
+    gap: 16px;
+
+    p {
+      width: 100%;
+    }
+  }
+`;
